55a23b4659eb7dd59332a747181ff8f9b74c6f18
Zakaria ben <zakariabenhanna2002@gmail.com>
2023-11-29 23:33:38 -0700

    .

--- app/main.js
+++ app/main.js
@@ -21,32 +21,40 @@ $(document).ready(function () {
     centerMode: true,
     centerPadding: 50,
     arrows: true,
-    prevArrow : '<button type="button" class="slick-prev"><</button>',
-    nextArrow : '<button type="button" class="slick-next">></button>',
-    mobileFirst: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">></button>',
+    // mobileFirst: true,
     responsive: [
-      {
-        breakpoint: 1440,
-        settings: {
-          slidesToShow: 3,
-          infinite: true,
+        {
+          breakpoint: 2660,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
         },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          infinite: true,
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
         },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
         },
-      },
-
-    ],
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+      
     // Add more settings as needed
   });
 });
