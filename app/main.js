03902e2a55e86f7165f0f16bdc1ba3c772f4dbd6
Zakaria ben <zakariabenhanna2002@gmail.com>
2023-11-30 18:44:08 -0700

    .

--- app/main.js
+++ app/main.js
@@ -25,35 +25,38 @@ $(document).ready(function () {
     nextArrow: '<button type="button" class="slick-next">></button>',
     // mobileFirst: true,
     responsive: [
-        {
-          breakpoint: 2660,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
+      {
+        breakpoint: 9999,
+        settings: {
+          
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
         }
-      ]
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      ,
+      {
+        breakpoint: 480,
+        
+         }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
       
     // Add more settings as needed
   });
